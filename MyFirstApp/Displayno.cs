c90d035e3d701c59797d731fc4ddf761e6d25f5d
skolekar558 <Skolekar558@gmail.com>
2024-04-12 15:13:53 +0530

    display

---
+++ MyFirstApp/Displayno.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstApp
+{
+    public class Displayno
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 1; i <= 50; i++)
+            {
+                if ((i % 3 == 0)&&(i%5==0) ){
+                    Console.WriteLine(i+"Welcome to pune");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine(i+"WELCOME");
+                }
+                else if(i % 3 == 0){
+                    Console.WriteLine(i+"PUNE");
+                }
+            }
+        }
+    }
+}
